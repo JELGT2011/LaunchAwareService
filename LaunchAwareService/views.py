9a4609e8423def5b97399a004c96fbdcfd06a4ec
Jason Libbey <jelgt2011@gmail.com>
2015-12-03 21:05:26 -0500

    added basic insert into db

--- LaunchAwareService/views.py
+++ LaunchAwareService/views.py
@@ -1,5 +1,7 @@
+
 from django.http import HttpResponse
 import json
+from datetime import datetime
 
 import LaunchAwareService.models
 
@@ -11,16 +13,22 @@ def index(request):
 
 
 def app_launch(request):
+
     launch_json = json.loads(request.body)
-    package_name = launch_json['packageName']
-    launch_time = launch_json['launchTime']
-    latitude = launch_json['locationInfo']['latitude']
-    longitude = launch_json['locationInfo']['longitude']
-    wifi_SSID = launch_json['wifiSSId']
+
+    package_name = str(launch_json['packageName'])
+    launch_time = datetime.strptime(launch_json['launchTime'], '%b %d, %Y %I:%M:%S %p')
+    latitude = float(launch_json['locationInfo']['latitude'])
+    longitude = float(launch_json['locationInfo']['longitude'])
+    wifi_SSID = str(launch_json['wifiSSId'])
 
     app_launch_data = AppLaunch(
-        package_name, launch_time, latitude, longitude, wifi_SSID
+        package_name=package_name,
+        launch_time=str(launch_time),
+        latitude=latitude,
+        longitude=longitude,
+        wifi_SSID=wifi_SSID
     )
     app_launch_data.save()
 
-    return HttpResponse(package_name + ' ' + launch_time)
+    return HttpResponse(package_name + ' ' + str(launch_time))
